4fcfd7a3f8a77072e0eb0923d1821d45a3b5de5e
canik6331 <canik6331@7HJM6Y1.Student.UGDSB.ED>
2020-03-10 14:16:37 -0400

    added comments and made the if else code block work as intended

--- src/firstforloops/FirstForLoops.java
+++ src/firstforloops/FirstForLoops.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *10/03/20
+ *cameron nikolasevic
+ *this program promts the user to choose a number 
+ *from 1-4 and tells the user what it should output.
+ *it will then output a counter in the console based
+ * on the users selection.
  */
 
 package firstforloops;
@@ -17,40 +20,42 @@ public class FirstForLoops {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //output the user selection
         Scanner keyboard = new Scanner(System.in);
         System.out.println("please enter your choice:");
         System.out.println("1... 0 to 10 by 1");
         System.out.println("2... 100 to 0 by 10");
         System.out.println("3... 50 to 500 by 50");
         System.out.println("4... 6000 to 1000 by 1000");
-        int input = keyboard.nextInt();
+        int input = keyboard.nextInt(); //user input
         
+        //decide the output
     if(input == 1){
         System.out.println("you have chosen 0 to 10 by 1");
         System.out.println("");
-        for(int i = 0; i >= 10; i++)
+        for(int i = 0; i <= 10; i++)
         {System.out.println(i);}   
     }
     else if(input == 2){
         System.out.println("You have chosen 100 to 0 by 10");
         System.out.println("");
-        for(int i= 100; i >= 100 ; i = i -10)
+        for(int i = 100; i >= 0 ; i-=10)
         {System.out.println(i);}        
     }
     else if (input == 3){
         System.out.println("You have chosen 50 to 500 by 50");
         System.out.println("");
-        for(int i = 50; i >=500 ; i = i + 50)
+        for(int i = 50; i <=500 ; i+=50)
         {System.out.println(i);}
     }
     else if (input == 4){
         System.out.println("You have chosen 6000 to 1000 by 1000");
         System.out.println("");
-        for(int i = 6000; i >= 1000; i = i + 1000)
+        for(int i = 6000; i >= 1000; i-=1000)
         {System.out.println(i);}
     }
     else{
-        System.out.println("invalid response");
+        System.out.println("Invalid response");
     } 
 
             
